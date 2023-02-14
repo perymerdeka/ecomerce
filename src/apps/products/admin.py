99461a7c7e847d70d18f45ebcdcc48e6434b9d08
perymerdeka <tkjasikk@gmail.com>
2023-02-14 15:17:02 +0700

    Register Admin

--- src/apps/products/admin.py
+++ src/apps/products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import CategoryModel, BrandModel, ProductModel
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    pass
+
+class BrandModelAdmin(admin.ModelAdmin):
+    pass
+
+class ProductModelAdmin(admin.ModelAdmin):
+    pass
+
+
+# register here
+admin.site.register(CategoryModel, CategoryModelAdmin)
+admin.site.register(BrandModel, BrandModelAdmin)
+admin.site.register(ProductModel, ProductModelAdmin)
